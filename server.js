9eea19ed2ad34484a02ae4719dccec00c5d6d615
pankajgusainn <pankajgusain990@gmail.com>
2026-08-31 00:38:16 +0530

    Containerize Node.js app with Docker Compose

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL = "mongodb://admin:qwerty@mongo:27017";
 const client = new MongoClient(MONGO_URL);
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
+    await client.connect();
     console.log('Connected successfully to server');
 
     const db = client.db("apnacollege-db");
@@ -26,7 +26,7 @@ app.get("/getUsers", async (req, res) => {
 app.post("/addUser", async (req, res) => {
     const userObj = req.body;
     console.log(req.body);
-    await client.connect(URL);
+    await client.connect();
     console.log('Connected successfully to server');
 
     const db = client.db("apnacollege-db");
